c7b6a30f452d051e6e15ec68edcc090c22b7aeeb
lightDev0405 <light.dev930405@gmail.com>
2024-08-28 00:26:09 +0300

    ..

--- src/llamafactory/data/processors/supervised.py
+++ src/llamafactory/data/processors/supervised.py
@@ -101,6 +101,7 @@ def _encode_supervised_example(
             return [], [] 
         # print("\n\n === TEXT | PRE-TRAIN ===\n\n", text)
         ##### input_ids = tokenizer.encode(text, add_special_tokens=False)
+        labels = [ IGNORE_INDEX ] + input_ids
         input_ids = [ tokenizer.bos_token_id ] + tokenizer.encode(text, add_special_tokens=False)
         # total_length = 1 if template.efficient_eos else 0
         # special_tokens_length = 1 if template.efficient_eos else 0
@@ -114,7 +115,6 @@ def _encode_supervised_example(
         if len(input_ids) >= cutoff_len:
             input_ids = input_ids[:cutoff_len]
         ##### labels = input_ids
-        labels = [IGNORE_INDEX] + input_ids
     # gotzmann | TRINITY ===		
 
     return input_ids, labels
