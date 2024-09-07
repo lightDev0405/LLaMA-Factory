fb74f83cdddaa0b6cc725476caa0e0ab534ddba7
lightDev0405 <light.dev930405@gmail.com>
2024-09-07 18:01:05 +0300

    ..

--- src/llamafactory/model/adapter.py
+++ src/llamafactory/model/adapter.py
@@ -255,9 +255,12 @@ def _setup_lora_tuning(
 
     # gotzmann Unsloth
     print("=== Upcasting embed_tokens to float32")
-    for param in model.parameters():
-        print("=== PARAM ===")
-        print(param)
+    #for param in model.parameters():
+    for name, param in model.named_parameters():
+        if not param.requires_grad: continue
+        if name.endswith("modules_to_save.default.weight"):
+            print(f"=== PARAM | {name} ===")
+            print(param)
     print("\n\n=== MODEL 1 ===\n\n")
     print(model)
     print("\n\n=== MODEL 2 ===\n\n")
